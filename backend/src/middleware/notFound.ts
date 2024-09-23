6a42701c317f3bf181dd34662766851ca69b5f90
Tayyab Ashraf <tayyabashraf22@gmail.com>
2024-09-23 06:19:29 +0500

    Removed static files

--- backend/src/middleware/notFound.ts
+++ backend/src/middleware/notFound.ts
@@ -5,6 +5,7 @@ const notFound = (req: Request, res: Response, next: NextFunction) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(NOT_FOUND);
     return res.json({
+        error,
         message: error.message,
     });
 }
